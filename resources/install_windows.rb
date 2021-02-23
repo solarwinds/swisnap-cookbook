56a8f987f75396f857c0b4061dbe789e06570e64
Dominik Maslyk <dominik.maslyk@solarwinds.com>
2021-02-23 18:07:13 +0100

    Add publisher temaptes to Chef

--- resources/install_windows.rb
+++ resources/install_windows.rb
@@ -7,15 +7,25 @@ action :install do
   windows_package 'solarwinds-snap-agent' do
     action :install
     installer_type :msi
-    source 'https://files.solarwinds.cloud/windows-solarwinds-snap-agent/solarwinds-snap-agent.msi' 
+    source 'https://files.solarwinds.cloud/windows-solarwinds-snap-agent/solarwinds-snap-agent.msi'
     options "TOKEN=#{new_resource.solarwinds_token} EULA=accept"
   end
 
   template 'C:\ProgramData\SolarWinds\Snap\config.yaml' do
     source 'config.yaml.erb'
-    notifies :restart, 'service[swisnapd]', :immediately
+    notifies :restart, 'service[swisnapd]', :delayed
   end
     
+  template 'C:\ProgramData\SolarWinds\Snap\plugins.d\publisher-appoptics.yaml' do
+    source 'publisher-appoptics.yaml.erb'
+    notifies :restart, 'service[swisnapd]', :delayed
+  end
+
+  template 'C:\ProgramData\SolarWinds\Snap\plugins.d\publisher-processes.yaml' do
+    source 'publisher-processes.yaml.erb'
+    notifies :restart, 'service[swisnapd]', :delayed
+  end
+
   service 'swisnapd' do
     action :enable
   end
