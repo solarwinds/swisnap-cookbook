20edc6efaa2d6ef13132b9cdd82dc1d89ee6b5fa
Dominik Maslyk <dominik.maslyk@solarwinds.com>
2021-03-01 16:23:23 +0100

    Add more containers to kitchen dokken

--- test/integration/default/snap_test.rb
+++ test/integration/default/snap_test.rb
@@ -6,15 +6,7 @@ describe file('/opt/SolarWinds/Snap/etc/config.yaml') do
   it { should exist }
 end
 
-describe port(21413) do
-  it { should be_listening }
-end
-
 describe service('swisnapd') do
   it { should be_enabled }
   it { should be_running }
-end
-
-describe file('/opt/SolarWinds/Snap/etc/plugins.d/docker.yaml') do
-  it { should exist }
 end
