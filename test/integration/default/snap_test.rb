fdfb21f6d2de31e42ed5a9c64a447e45680bafdf
dominik.maslyk <dominik.maslyk@solarwinds.com>
2021-03-04 16:19:44 +0100

    Test kichen

--- test/integration/default/snap_test.rb
+++ test/integration/default/snap_test.rb
@@ -2,11 +2,32 @@ describe package('solarwinds-snap-agent') do
   it { should be_installed }
 end
 
-describe file('/opt/SolarWinds/Snap/etc/config.yaml') do
-  it { should exist }
-end
-
 describe service('swisnapd') do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe file('/opt/SolarWinds/Snap/etc/config.yaml') do
+  it { should exist }
+  it { should be_owned_by 'solarwinds' }
+  its('group') { should eq 'solarwinds' }
+  its('mode') { should cmp '0640' }
+  its('content') { should match(%r{test-tags: test-kitchen}) }
+end
+
+describe file('/opt/SolarWinds/Snap/etc/plugins.d/publisher-processes.yaml') do
+  it { should exist }
+  it { should be_owned_by 'solarwinds' }
+  its('group') { should eq 'solarwinds' }
+  its('mode') { should cmp '0640' }
+  its('content') { should match(%r{hostname_alias: test-kitchen}) }
+end
+
+describe file('/opt/SolarWinds/Snap/etc/plugins.d/publisher-appoptics.yaml') do
+  it { should exist }
+  it { should be_owned_by 'solarwinds' }
+  its('group') { should eq 'solarwinds' }
+  its('mode') { should cmp '0640' }
+  its('content') { should match(%r{hostname_alias: test-kitchen}) }
+end
+
